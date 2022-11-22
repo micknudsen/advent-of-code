81ed6239b90c42f4807b94c00a73c1dbcb38e980
Michael Knudsen <micknudsen@gmail.com>
2022-11-22 20:22:22 +0100

    Implemented deliver_presents function

--- 2015/01/code.py
+++ 2015/01/code.py
@@ -1,5 +1,30 @@
 import unittest
 
+from typing import Iterable
+
+
+class InvalidInstructionError(Exception):
+    def __init__(self, instruction: str) -> None:
+        self.message = f"Invalid instruction: {instruction}"
+        super().__init__(self.message)
+
+
+def deliver_presents(instructions: Iterable[str]) -> int:
+    """Santa delivers presents starting at floor 0 based on `instructions`
+    given as a sequence of characters. Here "(" (resp. ")") means go one floor
+    up (resp. down). Any other instruction raises an exception. Return the
+    final floor number."""
+
+    floor = 0
+    for instruction in instructions:
+        if instruction == "(":
+            floor += 1
+        elif instruction == ")":
+            floor -= 1
+        else:
+            raise InvalidInstructionError(instruction=instruction)
+    return floor
+
 
 class TestCode(unittest.TestCase):
     def test_deliver_presents(self) -> None:
@@ -12,3 +37,7 @@ class TestCode(unittest.TestCase):
         self.assertEqual(deliver_presents(instructions="))("), -1)
         self.assertEqual(deliver_presents(instructions=")))"), -3)
         self.assertEqual(deliver_presents(instructions=")())())"), -3)
+
+    def test_deliver_presents_invalid_instruction(self) -> None:
+        with self.assertRaises(InvalidInstructionError):
+            deliver_presents(instructions=")[(")
