2b01db5c731c9f37728ecc65a35f90df9cf693cd
Michael Knudsen <micknudsen@gmail.com>
2022-11-27 19:38:00 +0100

    Test solution to first puzzle

--- 2015/02/code.py
+++ 2015/02/code.py
@@ -14,3 +14,18 @@ class TestCode(unittest.TestCase):
     def test_paper_needed(self) -> None:
         self.assertEqual(paper_needed(lenght=2, width=3, height=4), 58)
         self.assertEqual(paper_needed(lenght=1, width=1, height=10), 43)
+
+
+class TestPuzzle(unittest.TestCase):
+    def setUp(self) -> None:
+        with open("input.txt") as f:
+            self.dimensions = f.read().splitlines()
+
+    def test_part_one(self) -> None:
+        total_paper = 0
+        for dimension in self.dimensions:
+            lenght, width, height = map(int, dimension.split("x"))
+            total_paper += paper_needed(
+                lenght=lenght, width=width, height=height
+            )
+        self.assertEqual(total_paper, 1586300)
