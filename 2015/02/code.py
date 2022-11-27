e5e9ac8a4aa3b3f72376b0fe6d53cecb0252ad98
Michael Knudsen <micknudsen@gmail.com>
2022-11-27 19:32:53 +0100

    Implemented paper_needed function

--- 2015/02/code.py
+++ 2015/02/code.py
@@ -1,6 +1,15 @@
 import unittest
 
 
+def paper_needed(lenght: int, width: int, height: int) -> int:
+    """To wrap a box, the elves need to cover the surface area, and
+    for some reason they also need a little extra paper with an area
+    equals to the area of the smallest side of the box."""
+
+    areas = [lenght * width, width * height, height * lenght]
+    return 2 * sum(areas) + min(areas)
+
+
 class TestCode(unittest.TestCase):
     def test_paper_needed(self) -> None:
         self.assertEqual(paper_needed(lenght=2, width=3, height=4), 58)
