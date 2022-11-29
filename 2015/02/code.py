a378a7d17bc53d67c08c713b136a33592b391875
Michael Knudsen <micknudsen@gmail.com>
2022-11-29 20:30:03 +0100

    Added test of ribbon_needed function

--- 2015/02/code.py
+++ 2015/02/code.py
@@ -15,6 +15,10 @@ class TestCode(unittest.TestCase):
         self.assertEqual(paper_needed(lenght=2, width=3, height=4), 58)
         self.assertEqual(paper_needed(lenght=1, width=1, height=10), 43)
 
+    def test_ribbon_needed(self) -> None:
+        self.assertEqual(ribbon_needed(lenght=2, width=3, height=4), 34)
+        self.assertEqual(ribbon_needed(lenght=1, width=1, height=10), 14)
+
 
 class TestPuzzle(unittest.TestCase):
     def setUp(self) -> None:
