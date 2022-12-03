e9c8109bc9548afa181d27298d5c5dd8072b2738
Michael Knudsen <micknudsen@gmail.com>
2022-12-03 11:44:01 +0100

    Implemented mine_coin function

--- 2015/04/code.py
+++ 2015/04/code.py
@@ -1,4 +1,14 @@
+import hashlib
 import unittest
+
+
+def mine_coin(key: str) -> int:
+    number = 1
+    while True:
+        digest = hashlib.md5(f"{key}{number}".encode()).hexdigest()
+        if digest.startswith("00000"):
+            return number
+        number += 1
 
 
 class TestCode(unittest.TestCase):
