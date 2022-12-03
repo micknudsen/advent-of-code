f064c03ec671c16790527148044a25ee9f9be372
Michael Knudsen <micknudsen@gmail.com>
2022-12-03 10:20:16 +0100

    Added test of Coordinate class

--- 2015/03/code.py
+++ 2015/03/code.py
@@ -1,6 +1,11 @@
 import unittest
 
+
 class TestCode(unittest.TestCase):
+    def test_coordinate_equality(self):
+        self.assertEqual(Coordinate(x=1, y=1), Coordinate(x=1, y=1))
+        self.assertNotEqual(Coordinate(x=1, y=1), Coordinate(x=1, y=2))
+
     def test_presents_delivered(self) -> None:
         self.assertEqual(presents_delivered(directions=">"), 2)
         self.assertEqual(presents_delivered(directions="^>v<"), 4)
