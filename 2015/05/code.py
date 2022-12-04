ca308d85803142eb7cc5cb3059592076e6bd2d89
Michael Knudsen <micknudsen@gmail.com>
2022-12-04 20:19:00 +0100

    Implemented is_nice function

--- 2015/05/code.py
+++ 2015/05/code.py
@@ -1,6 +1,23 @@
 import unittest
 
 
+def is_nice(message: str) -> bool:
+
+    if sum(1 for char in message if char in "aeiou") < 3:
+        return False
+
+    if all(message[i] != message[i + 1] for i in range(len(message) - 1)):
+        return False
+
+    if any(
+        message[i : i + 2] in ["ab", "cd", "pq", "xy"]
+        for i in range(len(message) - 1)
+    ):
+        return False
+
+    return True
+
+
 class TestCode(unittest.TestCase):
     def test_is_nice(self) -> None:
         self.assertTrue(is_nice(message="ugknbfddgicrmopn"))
