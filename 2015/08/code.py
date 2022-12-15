6ba3c81b85a96881e66714705a4f8eb91fc5268d
Michael Knudsen <micknudsen@gmail.com>
2022-12-15 18:47:34 +0100

    Refactoring

--- 2015/08/code.py
+++ 2015/08/code.py
@@ -2,30 +2,18 @@ import codecs
 import unittest
 
 
-def representation_size(string: str) -> int:
-    """Return the size of the string representation. This exludes
-    quotes and respects escape characters."""
-    return len(codecs.decode(string[1:-1], "unicode-escape"))
-
-
-def memory_size(string: str) -> int:
-    """Return the size of the string in memory. This includes
-    both quotes and excape characters."""
-    return len(string)
+def literal(string: str) -> str:
+    """Return the string literal including
+    quotes and escape characters."""
+    return codecs.decode(string[1:-1], "unicode-escape")
 
 
 class TestCode(unittest.TestCase):
-    def test_representation_size(self) -> None:
-        self.assertEqual(representation_size(r'""'), 0)
-        self.assertEqual(representation_size(r'"abc"'), 3)
-        self.assertEqual(representation_size(r'"aaa\"aaa"'), 7)
-        self.assertEqual(representation_size(r'"\x27"'), 1)
-
-    def test_memory_size(self) -> None:
-        self.assertEqual(memory_size(r'""'), 2)
-        self.assertEqual(memory_size(r'"abc"'), 5)
-        self.assertEqual(memory_size(r'"aaa\"aaa"'), 10)
-        self.assertEqual(memory_size(r'"\x27"'), 6)
+    def test_literal(self) -> None:
+        self.assertEqual(literal(r'""'), r"")
+        self.assertEqual(literal(r'"abc"'), r"abc")
+        self.assertEqual(literal(r'"aaa\"aaa"'), r'aaa"aaa')
+        self.assertEqual(literal(r'"\x27"'), r"'")
 
 
 class TestPuzzles(unittest.TestCase):
@@ -35,9 +23,6 @@ class TestPuzzles(unittest.TestCase):
 
     def test_part_one(self) -> None:
         self.assertEqual(
-            sum(
-                memory_size(string) - representation_size(string)
-                for string in self.strings
-            ),
+            sum(len(string) - len(literal(string)) for string in self.strings),
             1333,
         )
