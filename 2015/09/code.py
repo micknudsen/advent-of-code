4cd828f597b255b1131f48abf0157bcd1c291a5f
Michael Knudsen <micknudsen@gmail.com>
2023-08-13 09:23:34 +0200

    Updated tests

--- 2015/09/code.py
+++ 2015/09/code.py
@@ -5,7 +5,7 @@ from typing import Dict
 
 class TestCode(unittest.TestCase):
     def setUp(self) -> None:
-        self.locations: Dict[str, Location] = locations_from_strings(
+        self.map = Map.init_from_strings(
             [
                 "London to Dublin = 464",
                 "London to Belfast = 518",
@@ -15,32 +15,32 @@ class TestCode(unittest.TestCase):
 
     def test_distance(self) -> None:
         self.assertEqual(
-            self.locations["London"].distance("Dublin"),
+            self.map.distance("London", "Dublin"),
             464,
         )
         self.assertEqual(
-            self.locations["Dublin"].distance("London"),
+            self.map.distance("Dublin", "London"),
             464,
         )
         self.assertEqual(
-            self.locations["London"].distance("Belfast"),
+            self.map.distance("London", "Belfast"),
             518,
         )
         self.assertEqual(
-            self.locations["Belfast"].distance("London"),
+            self.map.distance("Belfast", "London"),
             518,
         )
         self.assertEqual(
-            self.locations["Dublin"].distance("Belfast"),
+            self.map.distance("Dublin", "Belfast"),
             141,
         )
         self.assertEqual(
-            self.locations["Belfast"].distance_to("Dublin"),
+            self.map.distance("Belfast", "Dublin"),
             141,
         )
 
     def test_shortest_route(self) -> None:
         self.assertEqual(
-            shortest_route(self.locations),
+            map.shortest_route(),
             605,
         )
