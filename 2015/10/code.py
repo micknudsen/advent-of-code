fb6fbec1e5781afcc802226fd1da6d2d2dcd0046
Michael Knudsen <micknudsen@gmail.com>
2023-08-28 03:33:06 -0500

    Fewer lines

--- 2015/10/code.py
+++ 2015/10/code.py
@@ -21,26 +21,11 @@ def say(number: str) -> str:
 
 class TestCode(unittest.TestCase):
     def test_say(self) -> None:
-        self.assertEqual(
-            say(number="1"),
-            "11",
-        )
-        self.assertEqual(
-            say(number="11"),
-            "21",
-        )
-        self.assertEqual(
-            say(number="21"),
-            "1211",
-        )
-        self.assertEqual(
-            say(number="1211"),
-            "111221",
-        )
-        self.assertEqual(
-            say(number="111221"),
-            "312211",
-        )
+        self.assertEqual(say(number="1"), "11")
+        self.assertEqual(say(number="11"), "21")
+        self.assertEqual(say(number="21"), "1211")
+        self.assertEqual(say(number="1211"), "111221")
+        self.assertEqual(say(number="111221"), "312211")
 
 
 class TestPuzzles(unittest.TestCase):
