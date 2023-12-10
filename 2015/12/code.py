2ab07297377ac6a10a2699f62e84b389cc6d9e89
Michael Knudsen <micknudsen@gmail.com>
2023-12-10 15:37:33 +0100

    Implemented sum_numbers function

--- 2015/12/code.py
+++ 2015/12/code.py
@@ -1,4 +1,19 @@
+import json
 import unittest
+
+
+def sum_numbers(document: str) -> int:
+    data = json.loads(document)
+    result = 0
+    if isinstance(data, int):
+        return data
+    elif isinstance(data, list):
+        for entry in data:
+            result += sum_numbers(json.dumps(entry))
+    elif isinstance(data, dict):
+        for key in data:
+            result += sum_numbers(json.dumps(data[key]))
+    return result
 
 
 class TestCode(unittest.TestCase):
