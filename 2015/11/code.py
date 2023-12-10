5533fc912feb8b9e20923df3fa62f9559b73a68a
Michael Knudsen <micknudsen@gmail.com>
2023-12-10 08:17:13 +0100

    Implemented contains_increasing_straight function

--- 2015/11/code.py
+++ 2015/11/code.py
@@ -1,6 +1,15 @@
 import unittest
 
 
+def contains_increasing_straight(password: str) -> bool:
+    for i in range(len(password) - 2):
+        if ord(password[i]) + 1 == ord(password[i + 1]) and ord(password[i]) + 2 == ord(
+            password[i + 2]
+        ):
+            return True
+    return False
+
+
 class TestCode(unittest.TestCase):
     def test_contains_increasing_straight(self) -> None:
         self.assertTrue(contains_increasing_straight(password="hijklmmn"))
