6b5496bc0a269e816338afd6f953d7878c950e12
Michael Knudsen <micknudsen@gmail.com>
2023-12-10 08:14:55 +0100

    Added test of contains_increasing_straight function

--- 2015/11/code.py
+++ 2015/11/code.py
@@ -1,1 +1,8 @@
 import unittest
+
+
+class TestCode(unittest.TestCase):
+    def test_contains_increasing_straight(self) -> None:
+        self.assertTrue(contains_increasing_straight(password="hijklmmn"))
+        self.assertFalse(contains_increasing_straight(password="abbceffg"))
+        self.assertFalse(contains_increasing_straight(password="abbcegjk"))
